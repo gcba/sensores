d273565a3e9b9e0512a0b58316c2680d3c86a116
cristianReynaga <cristianreynaga@gmail.com>
2014-08-14 13:33:05 -0300

    update dht22

--- testStandAlone/testStandAlone.ino
+++ testStandAlone/testStandAlone.ino
@@ -14,23 +14,23 @@
 #include "DHT.h" //cargamos la librería DHT
 
 // Variables
-byte Offset=0;
+byte Offset = 0;
 #define DHTPIN 2 //Seleccionamos el pin en el que se //conectará el sensor
-#define DHTTYPE DHT11 //Se selecciona el DHT11 (hay //otros DHT)
+#define DHTTYPE DHT22 //Se selecciona el DHT11 (hay //otros DHT)
 DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino para comunicarse con el sensor
 unsigned long time;
 // Variables Luz
 int lightPin = A3;  //Pin de la foto-resistencia
-int light=0;     //Variable light
-int light0=0;
-float Res0=10.0;
+int light = 0;   //Variable light
+int light0 = 0;
+float Res0 = 10.0;
 //int min = 0;       //valor mínimo que da la foto-resistencia
 //int max = 1000;       //valor máximo que da la foto-resistencia
 // Variables ruido
 int electret = 0;
 int lect = 0;
-int noise = 0;   
-int threshold=450; //Valor medidio para el amplificador LM741
+int noise = 0;
+int threshold = 450; //Valor medidio para el amplificador LM741
 
 
 //=======================================================
@@ -47,19 +47,19 @@ void setup() {
 //=======================================================
 void loop()
 {
-    //Temperatura
-    int temp = dht.readTemperature()-6;
-    //Humedad
-    int hum = dht.readHumidity()+7;
-    //Ruido
-    int lect = analogRead(electret);    
-    noise = lect-threshold;
-    //Luz
-    light0 = analogRead(lightPin);   // Read the analogue pin
-    float Vout0=light0*0.0048828125;      // calculate the voltage
-    light=500/(Res0*((5-Vout0)/Vout0));
-    
-   Serial.print(temp, DEC);
+  //Temperatura
+  int temp = dht.readTemperature() - 6;
+  //Humedad
+  int hum = dht.readHumidity() + 7;
+  //Ruido
+  int lect = analogRead(electret);
+  noise = lect - threshold;
+  //Luz
+  light0 = analogRead(lightPin);   // Read the analogue pin
+  float Vout0 = light0 * 0.0048828125;  // calculate the voltage
+  light = 500 / (Res0 * ((5 - Vout0) / Vout0));
+
+  Serial.print(temp, DEC);
   Serial.print(",");
   Serial.print(hum, DEC);
   Serial.print(",");
